2c119395b4a6024dd7ca9a7596568c7348ebbb9d
NikittaThomas342 <nikita_tho@cmu.ac.th>
2021-05-28 17:22:56 +0700

    2.1

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-var newText = 'new Text'
-document.getElementById('ok').innerHTML = newText
-var namee = 'Nikita Thomas'
-document.getElementById('no').innerHTML = namee
+document.getElementById('cancel').addEventListener('mouseover', function (e){
+        console.log(e)
+    }
+)
+document.getElementById('cancel').addEventListener('click', function (e){
+    alert('Cancel is clicked')
+}
+)
+document.getElementById('ok').addEventListener('click', function (e){
+    alert('OK is clicked')
+}
+)
+document.getElementById('no').addEventListener('click', function (e){
+    alert('No is clicked')
+}
+)
